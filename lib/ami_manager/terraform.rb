38838002064b51a42a519192efb5380bc4441dc6
Chris Haueter and Derek Richard <drichard@pivotal.io>
2015-01-28 17:23:57 -0800

    Add Terraform (wrapper around terraform CLI) [#83232386]

---
+++ lib/ami_manager/terraform.rb
@@ -0,0 +1,37 @@
+require 'ami_manager'
+
+module AmiManager
+  class Terraform
+    CONFIG_DIR = File.join(AmiManager::ROOT, 'config', 'terraform')
+
+    def apply(vars)
+      command_string = [
+        'terraform apply',
+        vars_to_string(vars),
+        CONFIG_DIR
+      ].compact.join(' ')
+
+      system(command_string)
+    end
+
+    def force_destroy(vars)
+      command_string = [
+        'terraform destroy -force',
+        vars_to_string(vars),
+        CONFIG_DIR
+      ].compact.join(' ')
+
+      system(command_string)
+    end
+
+    private
+
+    def plan
+      `terraform plan #{CONFIG_DIR}`
+    end
+
+    def vars_to_string(vars)
+      vars.empty? ? nil : vars.map { |k,v| "-var '#{k}=#{v}'"}.join(' ')
+    end
+  end
+end
---
+++ spec/ami_manager/terraform_spec.rb
@@ -0,0 +1,65 @@
+require 'ami_manager/terraform'
+require 'spec_helper'
+
+module AmiManager
+  describe Terraform do
+    let(:terraform) { AmiManager::Terraform.new }
+    let(:config_dir) { Terraform::CONFIG_DIR }
+
+    describe 'configuration' do
+      it 'invokes the terraform CLI with the correct config dir' do
+        terraform_output = terraform.send(:plan)
+
+        expect($?.exitstatus).to eq(1)
+        expect(terraform_output).not_to match(/No Terraform configuration files found/)
+        expect(terraform_output).to match(/Required variable not set/)
+      end
+    end
+
+    describe '#apply' do
+      context 'with vars' do
+        let(:vars) { {some: 'var', another: 'var'} }
+
+        it 'invokes `terraform apply` with the args as vars' do
+          expect(terraform).to receive(:system).
+              with("terraform apply -var 'some=var' -var 'another=var' #{config_dir}").
+              and_return(true)
+
+          terraform.apply(vars)
+        end
+      end
+
+      context 'without vars' do
+        let(:vars) { {} }
+        it 'invokes `terraform apply`' do
+          expect(terraform).to receive(:system).with("terraform apply #{config_dir}")
+
+          terraform.apply(vars)
+        end
+      end
+    end
+
+    describe '#force_destroy' do
+      context 'with vars' do
+        let(:vars) { {some: 'var', another: 'var'} }
+
+        it 'invokes `terraform destroy -force` with the args as vars' do
+          expect(terraform).to receive(:system).
+              with("terraform destroy -force -var 'some=var' -var 'another=var' #{config_dir}").
+              and_return(true)
+
+          terraform.force_destroy(vars)
+        end
+      end
+
+      context 'without vars' do
+        let(:vars) { {} }
+        it 'invokes `terraform destroy -force`' do
+          expect(terraform).to receive(:system).with("terraform destroy -force #{config_dir}")
+
+          terraform.force_destroy(vars)
+        end
+      end
+    end
+  end
+end
